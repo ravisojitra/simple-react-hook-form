014e4bfb6cac73cc8420a8fa982fc673cf36468a
ravisojitra <ravisojitra79@gmail.com>
2020-05-24 21:35:40 +0530

    Updates

---
+++ precache-manifest.632cdf9db98a5d384da69f7542b922ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "301ef0d820736d36e4db19897143a95d",
+    "url": "/simple-react-hook-form/index.html"
+  },
+  {
+    "revision": "bc58f0307216e687b1e4",
+    "url": "/simple-react-hook-form/static/css/main.5a4d3cf3.chunk.css"
+  },
+  {
+    "revision": "50ffd73c104fd0b94a1f",
+    "url": "/simple-react-hook-form/static/js/2.171e4b5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-react-hook-form/static/js/2.171e4b5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc58f0307216e687b1e4",
+    "url": "/simple-react-hook-form/static/js/main.e4f0076f.chunk.js"
+  },
+  {
+    "revision": "9735a0219be8877a48f5",
+    "url": "/simple-react-hook-form/static/js/runtime-main.1d8c6975.js"
+  }
+]);
